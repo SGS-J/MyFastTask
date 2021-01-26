a5178213a60e07b305d1608730b4b0a5816a532f
SGS-J <santigarsan07@gmail.com>
2021-01-26 10:51:57 -0500

    Changed css framework to tailwindcs

---
+++ components/registerPage/RegisterForm.js
@@ -0,0 +1,146 @@
+import { useState } from 'react';
+import ColorPicker from '../ColorPicker';
+
+export default function RegisterForm(params) {
+   const [inputValues, setInputValues] = useState({
+      username: '',
+      password: '',
+      'conf-password': '',
+      gender: '',
+      birthday: '',
+      UIColor: '#D4C4FB',
+      avatar: 'unknown.png',
+   });
+
+   const handleOnChange = event => {
+      const { name, value } = event.target;
+      setInputValues({ ...inputValues, [name]: value });
+   };
+
+   const handleChangeColor = color => {
+      setInputValues({ ...inputValues, UIColor: color.hex });
+   };
+
+   const handleBlurConfPassword = event => {
+      const element = event.target;
+      const value = element.value;
+      if (value === inputValues.password) {
+         element.classList.remove('invalid');
+         element.classList.add('valid');
+      } else {
+         element.classList.remove('valid');
+         element.classList.add('invalid');
+      }
+   };
+
+   return (
+      <>
+         <div className="row container">
+            <form
+               style={{
+                  marginTop: '5em',
+               }}
+               className="col s12"
+            >
+               <div className="row">
+                  <div className="input-field col s12 m6">
+                     <i className="material-icons prefix">account_circle</i>
+                     <input
+                        name="username"
+                        id="username"
+                        type="text"
+                        className="validate"
+                        value={inputValues.username}
+                        onChange={handleOnChange}
+                        pattern=".{3,}"
+                        required
+                     />
+                     <label htmlFor="username">Username</label>
+                     <span
+                        className="helper-text"
+                        data-error="must have at least 3 characters"
+                        data-success="right"
+                     ></span>
+                  </div>
+
+                  <div className="row input-field col s12 m6">
+                     <h6 className="col s6 grey-text text-darken-1">Favorite Color:</h6>
+                     <div className="col s3">
+                        <ColorPicker color={inputValues.UIColor} onChangeHandler={handleChangeColor}/>
+                     </div>
+                  </div>
+
+                  <div className="input-field col s12 m6">
+                     <i className="material-icons prefix">vpn_key</i>
+                     <input
+                        name="password"
+                        id="password"
+                        type="password"
+                        className="validate"
+                        value={inputValues.password}
+                        onChange={handleOnChange}
+                        pattern=".{8,}"
+                        required
+                     />
+                     <label htmlFor="password">Password</label>
+                     <span
+                        className="helper-text"
+                        data-error="must have at least 8 characters"
+                        data-success="right"
+                     ></span>
+                  </div>
+
+
+                  <div className="input-field col s12 m6">
+                     <i className="material-icons prefix">face</i>
+                     <select
+                        name="gender"
+                        id="gender"
+                        onChange={handleOnChange}
+                     >
+                        <option value="" defaultValue>
+                           Choose your option
+                        </option>
+                        <option value="Male">Male</option>
+                        <option value="Female">Female</option>
+                        <option value="">Prefer not to say</option>
+                     </select>
+                     <label htmlFor="gender">Gender</label>
+                  </div>
+
+                  <div className="input-field col s12 m6">
+                     <i className="material-icons prefix">vpn_key</i>
+                     <input
+                        name="conf-password"
+                        id="conf-password"
+                        type="password"
+                        className="validate"
+                        value={inputValues['conf-password']}
+                        onChange={handleOnChange}
+                        onBlur={handleBlurConfPassword}
+                        required
+                     />
+                     <label htmlFor="conf-password">Confirm Password</label>
+                     <span
+                        className="helper-text"
+                        data-error="must match with password as above"
+                        data-success="right"
+                     ></span>
+                  </div>
+
+
+                  <div className="input-field col s12 m6">
+                     <input
+                        name="birthday"
+                        id="birthday"
+                        type="text"
+                        className="datepicker"
+                     />
+                     <label htmlFor="birthday">Birthdate</label>
+                  </div>
+               </div>
+            </form>
+         </div>
+      </>
+   );
+}
