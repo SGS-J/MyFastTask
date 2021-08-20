248dd22d781444271582cd851805a9c9d49d4587
santiago <santigarsan07@gmail.com>
2021-08-20 12:24:22 -0500

    Delete api folder

--- api/components/tasks/test.spec.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest')
-const app = require('../../app')
-
-describe('GET /tasks', function () {
-   it('Should return all tasks', function () {
-     request(app).get('/tasks').expect(200);
-   }) 
-})
